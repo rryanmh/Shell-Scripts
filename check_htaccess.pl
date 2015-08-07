1019b1df6a3aac662e8b2c55ef75be2aa840d91f
shard3 <ryanh-94@hotmail.com>
2015-08-07 17:14:50 +0100

    Create check_htaccess.pl
    NRPE plugin to check whether there's a htaccess on a certain link

---
+++ check_htaccess.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use Getopt::Std;
+getopts('h:');
+
+$output = `export HOME=/tmp; links $opt_h | grep Auth`;
+$auth = "Authorization Required";
+
+if(index($output, $auth) != -1){
+        print "OK: htaccess active for $opt_h";
+        exit (0);
+}else{
+        print "CRITICAL: htaccess not active for $opt_h";
+        exit (2);
+}
